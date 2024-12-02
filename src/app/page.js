e7d8160827c794b8e575727f318c072ad6251c27
HectornsGit <hector.novoa.segade@gmail.com>
2024-12-02 18:32:55 +0100

    feat: update styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,34 @@
 export default function Home() {
   return (
     <>
-      <header className="h-14 bg-[#170B1E] border-b-[#3D2343] border-b flex w-full items-center p-4">
+      <header className="h-16 bg-[--darkerpurple] border-b-[--cdarkpurple] border-b flex w-full items-center ">
         <nav className="text-[--cwhite] font-oswald font-light w-full">
-          <ul className="flex flex-row justify-between ">
-            <li className="py-4 px-2">
-              <h1 className="text-xl font-normal">Hectorns Dev</h1>
-            </li>
-            <li className=" justify-self-start items-center mr-2">
-              <ul className="flex gap-2">
-                <li className="border-b-[--clightpurple]  py-4 px-2 border-b-2">
-                  About Me
-                </li>
-                <li className="py-4 px-2">Projects</li>
-                <li className="py-4 px-2">Contact Me</li>
-                <li className="py-4 px-2">Other interests</li>
-              </ul>
-            </li>
+          <ul className="md:flex gap-4 hidden  justify-self-end items-center mr-2 text-lg">
+            <li className="border-b-[--clightpurple]  py-4 px-2 ">About Me</li>
+            <li className="py-4 px-2">Projects</li>
+            <li className="py-4 px-2">Contact Me</li>
+            <li className="py-4 px-2">Other interests</li>
           </ul>
+          <div className=" hidden absolute bg-[--cblack] z-10 w-full">
+            <ul className="flex flex-col items-center justify-center text-lg">
+              <li className="border-b-[--clightpurple]  py-4">About Me</li>
+              <li className="py-4 px-2">Projects</li>
+              <li className="py-4 px-2">Contact Me</li>
+              <li className="py-4 px-2">Other interests</li>
+            </ul>
+          </div>
         </nav>
       </header>
       <main>
         <header className="">
           <div>
             <div className="fixed xl:pl-12 xl:pt-10 pl-4 pt-4">
-              <h2 className="text-[--cwhite]  xl:text-7xl text-4xl  font-oswald font-normal w-full ">
+              <h1 className="text-[--cwhite]  xl:text-7xl text-4xl  font-oswald font-normal w-full ">
                 Héctor Novoa
-              </h2>
-              <h3 className="xl:text-2xl text-xs text-[--cyellow] text-right font-oswald font-light w-full">
+              </h1>
+              <h2 className="xl:text-2xl text-xs text-[--cyellow] text-right font-oswald font-light w-full">
                 Fullstack Dev
-              </h3>
+              </h2>
             </div>
             <img
               src="hero.jpg"
@@ -37,6 +36,7 @@ export default function Home() {
             ></img>
           </div>
         </header>
+        <section></section>
       </main>
     </>
   );
