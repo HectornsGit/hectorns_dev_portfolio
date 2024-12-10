36829b538200025cd2a9d10f784f98cb7f4c54bb
HectornsGit <hector.novoa.segade@gmail.com>
2024-12-10 19:47:24 +0100

    feat: adds an article to start working with

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 "use client";
-import { useState } from "react";
 import MainHeader from "../components/MainHeader";
 export default function Home() {
   return (
@@ -8,7 +7,7 @@ export default function Home() {
       <main>
         <header className="">
           <div>
-            <div className="fixed xl:pl-12 xl:pt-10 pl-4 pt-4">
+            <div className="absolute xl:pl-12 xl:pt-10 pl-4 pt-4">
               <h1 className="text-[--cwhite]  xl:text-7xl text-4xl  font-oswald font-normal w-full ">
                 Héctor Novoa
               </h1>
@@ -22,7 +21,28 @@ export default function Home() {
             ></img>
           </div>
         </header>
-        <section></section>
+        <section className="flex items-center flex-col">
+          <h3 className="my-8 text-2xl font-medium text-[--cpink] italic">
+            {"if("}
+            <span className="text-[--clightgreen]">
+              Something.interesting
+              {<span className="text-[--cpink]">{")"}</span>}
+            </span>
+            <span className=" text-[--clightblue]">{"=> "}</span>
+            <span className="text-[--clightblue]">{"Hector.try()"}</span>
+          </h3>
+          <article className="border-y flex md:flex-row flex-col justify-between border-[--clightpurple] gap-6 w-5/6 mx-20">
+            <p className="text-[--cturquoise] my-8 font-semibold w-4/6 ">
+              vaslkfghaskgbnklqbwgqpoi qbiogqbw bqkjb
+              qbwjgkbqpgbqwgbaslkfghaskgbnklqbwgqpoi qbiogqbw bqkjb
+              qbwjgkbqpgbqwgbaslkfghaskgbnklqbwgqpoi qbiogqbw bqkjb
+              qbwjgkbqpgbqwgbaslkfghaskgbnklqbwgqpoi qbiogqbw bqkjb
+              qbwjgkbqpgbqwgbaslkfghaskgbnklqbwgqpoi qbiogqbw bqkjb
+              qbwjgkbqpgbqwgb
+            </p>
+            <div className="w-40 h-40 m-4 bg-orange-700 grow "></div>
+          </article>
+        </section>
       </main>
     </>
   );
