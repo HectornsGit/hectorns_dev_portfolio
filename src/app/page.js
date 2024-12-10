ca5aaf9ff4144204172cb0ba4bc93380e14bd4bf
HectornsGit <hector.novoa.segade@gmail.com>
2024-12-10 17:44:02 +0100

    refactor: adds the MainHeader component, removes unused files and improves folder structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,10 @@
 "use client";
 import { useState } from "react";
+import MainHeader from "../components/MainHeader";
 export default function Home() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <>
-      <header className="h-16 bg-[--darkerpurple] border-b-[--cdarkpurple] border-b flex w-full items-center ">
-        <nav className="text-[--cwhite] font-oswald font-light w-full">
-          <ul className="md:flex gap-4 justify-self-end items-center mr-2 text-lg hidden">
-            <li className="border-b-[--clightpurple]  py-4 px-2 ">About Me</li>
-            <li className="py-4 px-2">Projects</li>
-            <li className="py-4 px-2">Contact Me</li>
-            <li className="py-4 px-2">Other interests</li>
-          </ul>
-          {window.innerWidth < 425 && (
-            <div className=" flex items-center justify-center bg-[--cblack] w-full">
-              <button className="">Menu</button>
-              <ul
-                className={
-                  "absolute bg-[--cblack]  z-10 flex flex-col items-center justify-center text-lg w-full " +
-                  (isMenuOpen
-                    ? " flex flex-col items-center top-14  absolute bg-[--cblack]"
-                    : "hidden")
-                }
-              >
-                <li className="border-b-[--clightpurple]  py-4">About Me</li>
-                <li className="py-4 px-2">Projects</li>
-                <li className="py-4 px-2">Contact Me</li>
-                <li className="py-4 px-2">Other interests</li>
-              </ul>
-            </div>
-          )}
-        </nav>
-      </header>
+      <MainHeader></MainHeader>
       <main>
         <header className="">
           <div>
---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+const MainHeader = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+  return (
+    <header className="h-16 bg-[--darkerpurple] border-b-[--cdarkpurple] border-b flex w-full items-center ">
+      <nav className="text-[--cwhite] font-oswald font-light w-full">
+        <ul className="md:flex gap-4 justify-self-end items-center mr-2 text-lg hidden">
+          <li className="border-b-[--clightpurple]  py-4 px-2 ">About Me</li>
+          <li className="py-4 px-2">Projects</li>
+          <li className="py-4 px-2">Contact Me</li>
+          <li className="py-4 px-2">Other interests</li>
+        </ul>
+
+        <div className=" md:hidden flex items-center justify-center bg-[--cblack] w-full">
+          <button className="">Menu</button>
+          <ul
+            className={
+              "absolute bg-[--cblack]  z-10 flex flex-col items-center justify-center text-lg w-full " +
+              (isMenuOpen
+                ? " flex flex-col items-center top-14  absolute bg-[--cblack]"
+                : "hidden")
+            }
+          >
+            <li className="border-b-[--clightpurple]  py-4">About Me</li>
+            <li className="py-4 px-2">Projects</li>
+            <li className="py-4 px-2">Contact Me</li>
+            <li className="py-4 px-2">Other interests</li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+export default MainHeader;
