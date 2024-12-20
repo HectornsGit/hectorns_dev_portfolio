70e16fc50d2e98af51f9b8502f573e9438bf0051
HectornsGit <hector.novoa.segade@gmail.com>
2024-12-20 13:58:22 +0100

    feat: adds responsive design for smartphone, tablet and laptop displays

--- src/app/page.js
+++ src/app/page.js
@@ -9,17 +9,17 @@ export default function Home() {
       <main>
         <header className="">
           <div>
-            <div className="absolute xl:pl-12 xl:pt-10 pl-4 pt-4 flex flex-col items-end">
-              <h1 className="text-[--cwhite]  xl:text-8xl text-4xl  font-oswald font-normal w-full ">
+            <div className="absolute xl:pl-12 md:pl-8 xl:pt-10 md:pt-8 pl-4 pt-4 flex flex-col items-end">
+              <h1 className="text-[--cwhite]  xl:text-8xl md:text-5xl text-4xl  font-oswald font-normal w-full ">
                 Héctor Novoa
               </h1>
-              <span className="xl:text-3xl  text-xs text-[--cyellow] text-right font-oswald font-extralight w-full">
+              <span className="xl:text-3xl md:text-xl  text-xs text-[--cyellow] text-right font-oswald font-extralight w-full">
                 Fullstack Developer
               </span>
             </div>
             <img
               src="hero.jpg"
-              className="xl:h-52 h-24 w-full object-right object-cover"
+              className="xl:h-52 md:h-32 h-24 w-full object-right object-cover"
             ></img>
           </div>
         </header>
--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,32 +1,32 @@
 const AboutMe = () => {
   return (
     <section className="ABOUT-ME">
-      <article className="mx-auto flex flex-col mt-8 justify-center w-4/6">
+      <article className="mx-auto flex flex-col mt-8 justify-center 2xl:w-4/6 xl:w-5/6 md:w-8/12 w-11/12">
         <header>
-          <h3 className="my-6 text-3xl font-semibold italic text-[--cyellow]  font-oswald">
+          <h3 className="my-6 xl:text-3xl md:text-xl font-semibold italic text-[--cyellow]  font-oswald">
             ABOUT ME
           </h3>
         </header>
-        <div className="flex">
-          <div className="w-2/3">
-            <p className="text-2xl text-[--cwhite] font-regular  mb-8 w-2/3 leading-relaxed  font-redhat">
-              I'm Héctor, a fullstack developer passionate about all kind of
+        <div className="flex xl:flex-row flex-col">
+          <div className=" xl:w-2/3  md:mx-0  mx-2 xl:mt-0 mt-4 order-2 text-[--cwhite] font-regular   xl:text-2xl md:text-xl  xl:leading-relaxed  font-redhat">
+            <p className="mb-8">
+              I{"'"}m Héctor, a fullstack developer passionate about all kind of
               graphic arts, games and music.
             </p>
-            <p className="text-2xl text-[--cwhite] font-regular  mb-8 w-2/3 leading-relaxed  font-redhat">
-              In my projects I worked in different areas: Backend, front, even
-              some basic devops and lead or organizative roles.
+            <p className=" mb-8">
+              In my projects I worked in different areas: Backend, frontend,
+              design, even some basic devops and lead or organizative roles.
             </p>
-            <p className="text-2xl text-[--cwhite] font-regular  mb-8 w-2/3 leading-relaxed  font-redhat">
+            <p className="mb-8">
               Now I can bring all this experience and passion to your team.
-              <span className=" pl-4 text-xl font-medium font-oswald italic text-cturquoise">
-                WANT TO KNOW MORE?
+              <span className="pl-4 xl:text-xl font-medium font-oswald italic text-cturquoise">
+                <br></br> WANT TO KNOW MORE?
               </span>
             </p>
           </div>
           <img
             src="./image.jpg"
-            className="max-w-1/3 h-72  mx-8 object-contain  rounded-lg grow "
+            className=" xl:h-72 md:h-64  xl:w-1/3 md:w-full  xl:self-auto md:self-center  xl:mr-8 2xl:object-contain md:object-cover  rounded-lg grow order-1"
           ></img>
         </div>
       </article>
