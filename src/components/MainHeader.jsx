76b4055bce3733077191fbe43592395741db3df9
HectornsGit <hector.novoa.segade@gmail.com>
2025-01-08 18:01:38 +0100

    Feat: adds scroll states and animations to header nav

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,47 +1,84 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const MainHeader = ({ experience, projects, contactMe }) => {
+  let initNeons = {
+    aboutMe: false,
+    contactMeMe: false,
+    projects: false,
+    experience: false,
+  };
+
+  const handleScroll = (ref, sectionName) => {
+    let neons = initNeons;
+    if (sectionName !== "aboutMe") {
+      ref?.current.scrollIntoView();
+    } else scrollTo(0, 0);
+
+    neons[sectionName] = true;
+    setNeonState({ ...neons });
+  };
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [neonState, setNeonState] = useState();
+
+  useEffect(
+    () =>
+      setNeonState({
+        aboutMe: true,
+        contactMeMe: false,
+        projects: false,
+        experience: false,
+      }),
+    []
+  );
+
+  if (
+    experience.current !== null &&
+    projects.current !== null &&
+    contactMe.current !== null
+  )
+    console.log(experience.current);
   return (
     <header className="h-16 bg-cblack border-b-cdarkpurple border-b flex w-full items-center fixed z-50 ">
       <nav className="text-[--cwhite] font-oswald font-light w-full   ">
         <ul className="md:flex gap-4 justify-self-end items-center mr-2 text-lg hidden">
-          <li className="border-b-[--clightpurple]  py-4 px-2  ">
-            <button
-              onClick={() => {
-                scrollTo(0, 0);
-              }}
-            >
+          <li className="border-b-[--clightpurple]py-4 px-2  ">
+            <button onClick={() => handleScroll(null, "aboutMe")}>
               About Me
             </button>
+            {neonState && (
+              <div className={neonState.aboutMe ? "neon" : ""}></div>
+            )}
           </li>
           <li className="py-4 px-2">
-            <button
-              onClick={() => {
-                experience.current.scrollIntoView();
-              }}
-            >
+            <button onClick={() => handleScroll(experience, "experience")}>
               Experience
             </button>
+            {neonState && (
+              <div className={neonState.experience ? "neon" : ""}></div>
+            )}
           </li>
           <li className="py-4 px-2">
-            <button
-              onClick={() => {
-                projects.current.scrollIntoView();
-              }}
-            >
+            <button onClick={() => handleScroll(projects, "projects")}>
               Projects
             </button>
+            {neonState && (
+              <div className={neonState.projects ? "neon" : ""}></div>
+            )}
           </li>
           <li className="py-4 px-2">
             <button
               onClick={() => {
-                contactMe.current.scrollIntoView();
+                handleScroll(contactMe, "contactMe");
               }}
             >
               Contact Me
             </button>
+            {neonState && (
+              <div className={neonState.contactMe ? "neon" : ""}></div>
+            )}
           </li>
           <li className="py-4 px-2">Other interests</li>
+          <div className="neon"></div>
         </ul>
 
         <div className=" md:hidden flex items-center justify-center bg-[--cblack] w-full">
